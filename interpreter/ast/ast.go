4b1350b35f7f121269fe10e1b85c8aaca8e85e7d
d0w <derekxu888@gmail.com>
2025-04-29 17:47:07 -0400

    add return statements

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "interpreter/token"
+import (
+	"bytes"
+	"interpreter/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -16,6 +20,7 @@ type Expression interface {
 	expressionNode()
 }
 
+// --- program ---
 type Program struct {
 	Statements []Statement
 }
@@ -28,6 +33,7 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+// --- let statements ---
 type LetStatement struct {
 	Token token.Token // token.LET
 	Name  *Identifier
@@ -39,6 +45,11 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	return ""
+}
+
+// --- identifier ---
 type Identifier struct {
 	Token token.Token // token.IDENT
 	Value string
@@ -47,4 +58,36 @@ type Identifier struct {
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+// --- return statements ---
+type ReturnStatement struct {
+	Token       token.Token // token.RETURN
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
+// --- expression statements ---
+type ExpressionStatement struct {
+	Token      token.Token // first token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
